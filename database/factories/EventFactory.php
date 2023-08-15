2a5fbbe45885214a6e05c863d50554e246c5c5ae
moonflag-vita <vitalis@moonflag.de>
2023-08-15 10:34:47 +0200

    Initial

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
+ */
+class EventFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->unique()->sentence(),
+            'description' => $this->faker->paragraph(),
+            'start_time' => $this->faker->dateTimeBetween('now', '+1 months'),
+            'end_time' => $this->faker->dateTimeBetween('+1 months', '+2 months'),
+        ];
+    }
+}
